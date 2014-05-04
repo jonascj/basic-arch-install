d039f2f719e1ac8506549d9c9c04b2e5e9c2c6d2
Jonas Camillus Jeppesen <wuhtzu@gmail.com>
2014-05-04 14:52:00 +0200

    moved copy of archinstall2.sh after pacstrap

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 
 
 
-## Place the archinstall2.sh script
-## at the root of the new filesystem
-cp ./archinstall2.sh /mnt/archinstall2.sh
-cp ./grub.cfg /mnt/grub.cfg
 
 # Execute permission on the two scripts
 chmod +x archinstall1.sh
@@ -13,7 +9,13 @@ chmod +x /mnt/archinstall2.sh
 
 
 # Before arch-chroot
-./archinstall1.sh 2>&1 | tee /mnt/archinstall1.log
+./archinstall1.sh 2>&1 | tee ./archinstall1.log
+
+## Place the archinstall2.sh script and grub.cfg
+## at the root of the new filesystem (/mnt)
+cp ./archinstall2.sh /mnt/archinstall2.sh
+cp ./grub.cfg /mnt/grub.cfg
+cp ./archinstall1.log /mnt/archinstall1.log
 
 # After arch-chroot
 arch-chroot /mnt /bin/sh -c "/archinstall2.sh 2>&1 | tee /archinstall2.log"
