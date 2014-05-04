f251838dc21e7358e2c45f62ce4c7a62f5213beb
Jonas Camillus Jeppesen <wuhtzu@gmail.com>
2014-05-04 14:56:32 +0200

    chmod moved after cp of archinstall2.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-
-
-
-# Execute permission on the two scripts
+## Execute permission 
 chmod +x archinstall1.sh
-chmod +x /mnt/archinstall2.sh
 
 
-# Before arch-chroot
+## Before arch-chroot
 ./archinstall1.sh 2>&1 | tee ./archinstall1.log
 
 ## Place the archinstall2.sh script and grub.cfg
@@ -17,10 +13,14 @@ cp ./archinstall2.sh /mnt/archinstall2.sh
 cp ./grub.cfg /mnt/grub.cfg
 cp ./archinstall1.log /mnt/archinstall1.log
 
-# After arch-chroot
+# Execute permission on archinstall2.sh
+chmod +x /mnt/archinstall2.sh
+
+
+## After arch-chroot
 arch-chroot /mnt /bin/sh -c "/archinstall2.sh 2>&1 | tee /archinstall2.log"
 
-# remove the archinstall2.sh script from /mnt (the new root)
+## remove the archinstall2.sh script from /mnt (the new root)
 rm -f /mnt/archinstall2.sh
 
 # unmount and restart
