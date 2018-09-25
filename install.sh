0b9c0213cd7036054daff82ae2d9c7e8a8cb991d
Jonas Camillus Jeppesen <jonascj@camillus.dk>
2018-09-25 21:58:34 +0200

    Fixed naming of scripts in comments

--- install.sh
+++ install.sh
@@ -11,22 +11,19 @@ chmod +x outside-arch-chroot.sh
 ## Before arch-chroot
 ./outside-arch-chroot.sh #2>&1 | tee ./tmp/archinstall1.log
 
-## Place the archinstall2.sh script and grub.cfg
+
+## Place the inside-arch-chroot.sh script
 ## at the root of the new filesystem (/mnt)
 cp ./inside-arch-chroot.sh /mnt
-cp ./grub.cfg /mnt
-
-# Execute permission on archinstall2.sh
-chmod +x /mnt/inside-arch-chroot.sh
-
+chmod +x /mnt/inside-arch-chroot.sh 
 
 ## Inside chroot
 arch-chroot /mnt /bin/bash -c /inside-arch-chroot.sh
 
-## remove the archinstall2.sh script from /mnt (the new root)
+## remove the inside-arch-chroot.sh script from /mnt (the new root)
 rm -f /mnt/inside-arch-chroot.sh
 
-# unmount and restart
+# Unmount and restart
 umount -R /mnt
 
 read -q "key?Reboot [Yyn]? "
